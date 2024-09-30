cacb3706c9bd2b18a6027b7126628f1421307f91
Gustavo <gustavo.gomes.silva13@escola.pr.gov.br>
2024-09-30 11:26:40 -0300

    iniciando os arquivos do java

---
+++ src/Condicional.java
@@ -0,0 +1,10 @@
+public class Condicional {
+    public static void main(String[] args) {
+        int anoDeLancamento = 2022;
+        boolean incluindoNoPlano = false;
+        double notaDoFilme = 8.1;
+        string tipoPlano = "plus";
+
+        if (anoDeLancamento >= 2022) {
+    }
+}
